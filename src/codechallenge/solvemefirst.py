31e5531fb9a4d9bbb43b5b4d55a3b518398cde5b
Vignesh-Tech14 <Vignesh.vellore@proton.me>
2024-09-29 20:24:55 +0530

    my first code

---
+++ src/codechallenge/solvemefirst.py
@@ -0,0 +1,7 @@
+def solveMeFirst(a,b):
+    return a+b
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+res = solveMeFirst(num1,num2)
+print("The sum of the numbers are: ",res)
